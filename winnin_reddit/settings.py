a7586ce2167b91776fef083ab4611e9d4a40aa59
Igor Santos <igorbpf@gmail.com>
2018-04-12 13:03:11 -0300

    chore: setup env vars

--- winnin_reddit/settings.py
+++ winnin_reddit/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 # import datetime
-# from decouple import config, Csv
-# import dj_database_url
+from decouple import config, Csv
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*%9gqegfeqf+@w8kc68kc3uih!acir301!t$+6ei-fm-n$g*7y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -77,12 +78,10 @@ WSGI_APPLICATION = 'winnin_reddit.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
