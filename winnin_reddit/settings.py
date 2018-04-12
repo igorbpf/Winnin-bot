deabd08cd2e1fe41bfed580bdfad3d76a499a016
Igor Santos <igorbpf@gmail.com>
2018-04-12 17:43:06 -0300

    chore: test cron

--- winnin_reddit/settings.py
+++ winnin_reddit/settings.py
@@ -147,7 +147,7 @@ CELERYBEAT_SCHEDULE = {
     'reddit_bot': {
         'task': 'bot.utilities.reddit_bot.get_posts',
         'schedule': crontab(
-            minute='22',
+            minute='44',
             hour='20',
         )
     },
