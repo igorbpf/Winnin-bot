e856201fadc11ec521f7f2a61e75286650a68b22
Igor Santos <igorbpf@gmail.com>
2018-04-12 17:19:48 -0300

    chore: change cron time 2

--- winnin_reddit/settings.py
+++ winnin_reddit/settings.py
@@ -147,7 +147,7 @@ CELERYBEAT_SCHEDULE = {
     'reddit_bot': {
         'task': 'bot.utilities.reddit_bot.get_posts',
         'schedule': crontab(
-            minute='18',
+            minute='22',
             hour='20',
         )
     },
