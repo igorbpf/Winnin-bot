7d789578e18b734e9cfdf06d4cc9f3a72aba5853
Igor Santos <igorbpf@gmail.com>
2018-04-12 21:16:51 -0300

    chore: remove files

--- winnin_reddit/settings.py
+++ winnin_reddit/settings.py
@@ -147,7 +147,8 @@ CELERYBEAT_SCHEDULE = {
     'reddit_bot': {
         'task': 'bot.utilities.reddit_bot.get_posts',
         'schedule': crontab(
-            minute='*/5',
+            minute='20',
+            hour='0',
         )
     },
 }
